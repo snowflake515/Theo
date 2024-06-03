435782b645d881d4abd3d1d47602e4f3b614936e
snowflake515 <davidviera0515@gmail.com>
2024-06-03 08:40:33 +0800

    aaa

--- application/logs/log-2024-04-11.php
+++
@@ -1,9 +0,0 @@
-ERROR - 2024-04-11 03:15:36 --> Severity: Notice  --> Undefined variable: id_appt D:\P_project\tablet\application\views\template2\templte_css_js.php 467
-ERROR - 2024-04-11 03:15:48 --> Severity: Notice  --> Undefined variable: id_appt D:\P_project\tablet\application\views\template2\templte_css_js.php 467
-ERROR - 2024-04-11 03:30:43 --> Severity: Warning  --> file_get_contents(http://well1-wt1theo2.abe01.viawesthosted.net:8080/api/session/generate-careplan-new/0/en): failed to open stream: No connection could be made because the target machine actively refused it.
- D:\P_project\tablet\application\controllers\template_v2.php 48
-ERROR - 2024-04-11 05:21:00 --> Severity: Warning  --> pg_connect(): Unable to connect to PostgreSQL server: FATAL:  no pg_hba.conf entry for host &quot;188.43.253.75&quot;, user &quot;postgres&quot;, database &quot;noblemd20&quot;, SSL off D:\P_project\theo_project\system\database\drivers\postgre\postgre_driver.php 86
-ERROR - 2024-04-11 05:21:00 --> Unable to connect to the database
-ERROR - 2024-04-11 05:21:00 --> Severity: Warning  --> pg_query() expects parameter 1 to be resource, boolean given D:\P_project\theo_project\system\database\drivers\postgre\postgre_driver.php 176
-ERROR - 2024-04-11 05:21:00 --> Severity: Warning  --> pg_num_rows() expects parameter 1 to be resource, null given D:\P_project\theo_project\system\database\drivers\postgre\postgre_result.php 37
-ERROR - 2024-04-11 05:21:00 --> Severity: Warning  --> pg_num_rows() expects parameter 1 to be resource, null given D:\P_project\theo_project\system\database\drivers\postgre\postgre_result.php 37
